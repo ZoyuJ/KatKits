86ad29674065c81bea8767502b53aa3f3e433289
ZoyuJ <ZoyuJ@hotmal.com>
2020-12-28 14:07:27 +0800

    add commandline args extract（for .net framework）

---
+++ KatKits.Framework/Process.cs
@@ -0,0 +1,18 @@
+﻿#if FRAMEWORK
+namespace KatKits {
+  using System.Diagnostics;
+  using System.Linq;
+  using System.Management;
+
+  public static partial class KatKits {
+    public static string GetCommandLineArgs(this Process Process) {
+      using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + Process.Id))
+      using (ManagementObjectCollection objects = searcher.Get()) {
+        return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+      }
+
+    }
+  }
+}
+
+#endif
---
+++ KatKitsCommon/Process.cs
@@ -0,0 +1,19 @@
+﻿#if FRAMEWORK
+namespace KatKits {
+  using System;
+  using System.Collections.Generic;
+  using System.Diagnostics;
+  using System.Text;
+
+  public static partial class KatKits {
+    private static string GetCommandLine(this Process process) {
+      using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+      using (ManagementObjectCollection objects = searcher.Get()) {
+        return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+      }
+
+    }
+  }
+}
+
+#endif
